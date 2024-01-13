7048d03fbe6828059d8a7acf028fd038437a1857
tekporlipos <dzikunujohn36@gmail.com>
2024-01-13 22:40:42 +0000

    feat: models added

--- src/models/NoteModel.py
+++ src/models/NoteModel.py
@@ -1,10 +1,19 @@
+import uuid
+
+from sqlalchemy.orm import relationship
+
 from app import db
 
 
-class Trucks(db.Model):
-    id = db.Column(db.String(10), primary_key=True)
-    provider_id = db.Column(db.Integer, db.ForeignKey('provider.id'))
-    provider = db.relationship('Provider', backref=db.backref('trucks', lazy=True))
+class Notes(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    user_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
+    user = relationship('User', backref='notes')
+    title = db.Column(db.String(50), unique=True, nullable=False)
+    body = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
     def __repr__(self):
-        return f"<Trucks(id={self.id}, provider_id={self.provider_id})>"
+        return (f"<Notes(id={self.id}, user_id={self.user_id}, user={self.user}, title={self.title}, body={self.body},"
+                f"created_at={self.created_at}, updated_at={self.updated_at})>")
